74f10f98c4abe9504733a65bdb179a00f066093d
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2024-02-27 23:00:35 -0800

    resolve cherry-pick conflict

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Quantum Image Processing setup file."""
+
+from setuptools import setup
+
+setup(
+    name="quantum_image_processing",
+    version="0.1",
+    description="Quantum Image Processing",
+    author="Saasha Joshi",
+    author_email="saashajoshi08@gmail.com",
+    url="https://github.com/SaashaJoshi/quantum-image-processing",
+    packages=["quantum_image_processing"],
+)
---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+"""conftest.py"""
+
+# pylint: disable=unused-import
+from tests.data_encoder.image_representations.test_frqi import (
+    circuit_pixel_position_fixture,
+)
+from tests.tensor_networks.test_mps import parameterization_mapper_fixture
