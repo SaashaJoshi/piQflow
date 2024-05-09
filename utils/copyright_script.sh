e1553e5aba76d2d78d8bbbf601a15b67cd167de3
SaashaJoshi <saashajoshi08@gmail.com>
2024-05-09 14:13:33 -0700

    remove extra files. a mess with cherry-pick :(

--- utils/copyright_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-copyright_notice=$(cat copyright.txt)
-
-for file in $(git ls-files | grep '\.py$'); do
-  # Prepend the copyright notice to the Python file
-  (echo "$copyright_notice" && cat "$file") > "$file.tmp"
-  mv "$file.tmp" "$file"
-done
